3ea8dae6da367703bd3074966e0e861a80616560
SamuelVera <samuelvera1999@gmail.com>
2018-03-09 23:21:16 -0400

    Clase Cliente

---
+++ src/ManejadorDeLaApp/Cliente.java
@@ -0,0 +1,86 @@
+package ManejadorDeLaApp;
+
+import CodigoEstructuras.ListaSimple;
+import CodigoEstructuras.NodoSimple;
+
+public class Cliente {
+    
+    private int ci, telefono;
+    private ListaSimple tickets;
+    private boolean solvente;
+    
+    public Cliente(int ci, int telefono){   //Constructor con datos del cliente
+        this.ci = ci;
+        this.telefono = telefono;
+    }
+    
+        //Agregar n tickets al carrito
+    public void agregarTickets(Ticket ticket, int cantidad){
+        
+        while(cantidad != 0){
+            this.tickets.insertPrimero(ticket);
+            cantidad--;
+        }
+        
+    }
+    
+        //Método de ver si todos los tickets estan pagados
+    public void ticketsPagados(){
+        
+        int temp = 0;
+        NodoSimple aux = this.tickets.getCabeza();
+        
+        while(aux != null){
+            if(((Ticket)aux.getDato()).isPagado() == false){
+                this.solvente = false;
+            }else{
+                ((Ticket)aux.getDato()).getSala().agregarTicketVendido();
+                temp++;
+            }
+            aux = aux.getProximo();
+        }
+        
+        if(temp == this.tickets.contarElementos()){
+            this.solvente = true; //Todos los tickets fueron pagados
+        }
+        
+    }
+    
+        //Pagar n Tickets conociendo sus identificadores
+    public void pagar(int[] id){
+        
+        NodoSimple aux = this.tickets.getCabeza();
+        
+        while(aux != null){
+            for(int i=0;i<id.length;i++){
+                if(!((Ticket)aux.getDato()).isPagado() && id[i] == ((Ticket)aux.getDato()).getId()){
+                    ((Ticket)aux.getDato()).setPagado(true);
+                    break;
+                }
+            }
+            aux = aux.getProximo();
+        }
+        
+        ticketsPagados();
+        
+    }
+    
+        //Retirar n Tickets conociendo sus identificadores
+    public void deshacer(int[] id){
+        
+        NodoSimple aux = this.tickets.getCabeza();
+        int pos = 0;
+        
+        while(aux != null){
+            for(int i=0;i<id.length;i++){
+                if(id[i] == ((Ticket)aux.getDato()).getId()){
+                    this.tickets.eliminarPosicion(pos);
+                    pos--;
+                    break;
+                }
+            }
+            pos++;
+        }
+        
+    }
+}
